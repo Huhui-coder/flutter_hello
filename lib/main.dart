f228ad88954b0c70ad86dae9a091caa9f0c87a38
huhui <569263082@qq.com>
2020-03-24 23:06:54 +0800

    fix： 增加备注

--- lib/main.dart
+++ lib/main.dart
@@ -6,34 +6,37 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //使用MaterialApp 自身的一些属性
     return MaterialApp(
+      // 设置主体内容，内容是使用home这个类去承载的
       home: Home(),
+      // 设置主题
       theme: ThemeData(primaryColor: Colors.yellow),
     );
   }
 }
 
 class Home extends StatelessWidget {
+  // 构建一个Widget。两个参数 一个context 一个 index
   Widget _listItemBuilder(BuildContext context, int index) {
     return Container(
-      color:Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height:16.0),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.subtitle1
-          ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subtitle2
-          ),
-          SizedBox(height:16.0),
-        ],
-        )
-    );
+      // 设置这个容器的颜色
+        color: Colors.white,
+        // 设置这个容器的边距
+        margin: EdgeInsets.all(8.0),
+        // child 去规划内容，使用的是一个列的Column布局
+        child: Column(
+          // 使用children 去规划内容,列表项是一个数组
+          children: <Widget>[
+            Image.network(posts[index].imageUrl),
+            SizedBox(height: 16.0),
+            Text(posts[index].title,
+                style: Theme.of(context).textTheme.subtitle1),
+            Text(posts[index].author,
+                style: Theme.of(context).textTheme.subtitle2),
+            SizedBox(height: 16.0),
+          ],
+        ));
   }
 
   @override
@@ -43,11 +46,13 @@ class Home extends StatelessWidget {
           title: Text('hello'),
           elevation: 0.0,
         ),
+        // body 主题内容是使用ListView.builder 去构建一个列表
         body: ListView.builder(
+          // itemCount：数量，itemBuilder ： 构建列表的UI表示
             itemCount: posts.length, itemBuilder: _listItemBuilder));
   }
 }
-
+// 这个是完全没用到的
 class Hello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
